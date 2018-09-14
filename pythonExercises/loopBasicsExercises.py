532bebc93f2c4f4476094cc800ab154657359c92
atilla <icantiemyshoe@yahoo.com>
2018-09-14 16:16:22 -0400

    initial commit

---
+++ pythonExercises/loopBasicsExercises.py
@@ -0,0 +1,25 @@
+import os
+
+
+class loopBasicsClass(object):
+    def __init__(self):
+        self.my_array = ['first if value print', 'second if value print', 'third if value print']
+        self.my_while_array = ['first while value print', 'second while value print', 'third while value print']
+
+    def ifCondition(self):
+        ### Your Code Here ###
+        #
+        if true:
+            print(true)
+        else:
+            print(false)
+
+    def forLoop(self):
+        ### Your Code Here ###
+
+            print(self.my_array)
+
+    def whileLoop(self):
+        ### Your Code Here ###
+
+            print(self.my_while_array)
---
+++ pythonExercises/objectBasicsExercises.py
@@ -0,0 +1,15 @@
+import os
+
+class objectBasicsClass(object):
+    def __init__(self):
+        # Create a string
+        ### Your Code Here ###
+        my_str =
+
+        # Create a list
+        ### Your Code Here ###
+        my_list =
+
+        # Create a dict
+        ### Your Code Here ###
+        my_dict =
